d447299ce6a2cc675a5877e99a44bfa21380791d
gzm <1097831215@qq.com>
2018-01-16 14:03:57 +0800

    Journal

---
+++ Journal/Journal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal
+//
+//  Created by huaguang on 2018/1/8.
+//  Copyright © 2018年 GongZhimMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Journal/Journal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal
+//
+//  Created by huaguang on 2018/1/8.
+//  Copyright © 2018年 GongZhimMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
